3417c2f824d1b103911143d5f8a47bb15479ff7e
Sajib Hossain <sajib1066@gmail.com>
2021-12-20 20:38:51 +0600

    update exercise 03

--- exercise/ex03.c
+++ exercise/ex03.c
@@ -5,10 +5,12 @@ Judge the prime numbers between 101 and 200 and output them.
 */
 
 #include<stdio.h>
+#define START_NUMBER 101
+#define END_NUMBER 200
 
 int main() {
-    printf("Prime numbers between 101 and 200 are:\n");
-    for (int i = 101; i <= 200; i++) {
+    printf("Prime numbers between %d and %d are:\n", START_NUMBER, END_NUMBER);
+    for (int i = START_NUMBER; i <= END_NUMBER; i++) {
         int flag = 0;
         for (int j = 2; j <= i / 2; j++) {
             if (i % j == 0) {
@@ -17,8 +19,9 @@ int main() {
             }
         }
         if (flag == 0) {
-            printf("%d\n", i);
+            printf("%d, ", i);
         }
     }
+    printf("\n");
     return 0;
 }

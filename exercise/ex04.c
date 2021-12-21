e1c994a1b3fe817cd3c7984727782575f1ef0291
Sajib Hossain <sajib1066@gmail.com>
2021-12-21 23:49:06 +0600

    add excercise 04

---
+++ exercise/ex04.c
@@ -0,0 +1,34 @@
+// Author: Md Sajib Hossain
+/*
+Problem:
+Print all "Narcissus Numbers". The so-called "Narcissus Number"
+refers to a three-digit number whose sum of each digital cube is equal to
+that number. For example, 153=13+53+33，so 153 is a "Narcissus Number".
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    int number, original_number, reminder, sum=0, digit=0;
+    printf("Please Enter a three-digit number: ");
+    scanf("%d", &number);
+    original_number = number;
+
+    while(number != 0) {
+        digit++;
+        number = number / 10;
+    }
+    number = original_number;
+    while(number != 0) {
+        reminder = number % 10;
+        sum = sum + pow(reminder, digit);
+        number = number / 10;
+    }
+    if (sum == original_number) {
+        printf("%d is a Narcissus Number\n", original_number);
+    } else {
+        printf("%d is not a Narcissus Number\n", original_number);
+    }
+    return 0;
+}

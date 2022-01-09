013e90a45c1a7b71ffb1c4f14e2c41c14386d788
Sajib Hossain <sajib1066@gmail.com>
2022-01-10 00:13:57 +0600

    complete exercise 08

---
+++ exercise/ex08.c
@@ -0,0 +1,27 @@
+// Author: Md Sajib Hossain
+/*
+    Problem:
+    Input two positive integers m and n to find the greatest common divisor.
+*/
+
+#include<stdio.h>
+
+int main() {
+    int m, n, temp, a, b, c;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &m, &n);
+    if(m > n) {
+        temp = n;
+        n = m;
+        m = temp;
+    }
+    a = m, b = n;
+    while(b != 0) {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+    c = m * n / a;
+    printf("The least common multiple is %d\n", c); 
+    return 0;
+}

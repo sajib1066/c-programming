6d5733e4fbb3d199c4e49868efc238c28d09645b
Sajib Hossain <sajib1066@gmail.com>
2022-03-01 15:33:45 +0800

    add iteration value

--- mathematics_2/euler_method.c
+++ mathematics_2/euler_method.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-#define f(x,y) 1 + y  // that is the function change it to your function
+#define f(x,y) y - 1  // that is the function change it to your function
 
 int main() {
     float x0, y0, dx, h, yn, slope;
@@ -10,7 +10,7 @@ int main() {
     scanf("%f", &x0);
     printf("y0 = ");
     scanf("%f", &y0);
-    printf("Enter calculation point dx = ");
+    printf("Enter step size dx = ");
     scanf("%f", &dx);
     printf("Enter number of steps: ");
     scanf("%d", &n);
@@ -19,18 +19,19 @@ int main() {
     h = (dx-x0);
 
     /* Euler's Method */
-    printf("\nx0\ty(Euler)\n");
-    printf("-------------------\n");
+    printf("\nn\tx0\ty(Euler)\n");
+    printf("------------------------\n");
     for(i=0; i < n; i++) {
         slope = f(x0, y0);
         yn = y0 + h * slope;
-        printf("%.5g\t%.5g\n",x0,y0);
+        printf("%d\t%.5g\t%.5g\n",i+1, x0,y0);
         y0 = yn;
         x0 = x0+h;
     }
 
+    printf("------------------------\n");
     /* Displaying result */
-    printf("\nValue of y at x = %0.2g is %0.3g\n",dx, yn);
+    /* printf("\nValue of y at x = %0.2g is %0.3g\n",dx,yn); */
 
     return 0;
 }
